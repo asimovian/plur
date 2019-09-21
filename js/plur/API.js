da931add8fd001735f663e5fb3b9cc46af7476a6
roylaurie <roy.laurie@gmail.com>
2019-09-20 20:30:37 -0700

    api

--- js/plur/API.js
+++ js/plur/API.js
@@ -4,33 +4,34 @@
  * @requires plur/PlurObject
  * @module plur/PlurAPI
  */
-define([
-    'plur/PlurObject' ],
-function(
-    PlurObject ) {
+'use strict';
+
+import { PlurObject } from 'plur/PlurObject';
+
 
 /**
  * API version information
  *
  * @class PlurAPI
  * @alias {module:plur/PlurAPI}
+ * @final
  **
  */
 class PlurAPI {
     constructor(version, scmUrl, branch) {
-        this.version = version;
-        this.scmUrl = scmUrl;
-        this.branch = branch;
+        PlurObject.constProperty(this, 'version', version);
+        PlurObject.constProperty(this, 'scmUrl', scmUrl;
+        PlurObject.constProperty(this, 'branch', branch;
     };
 }
 
 PlurObject.plurify('plur/plurAPI', PlurAPI);
 
-PlurAPI.singleton = new PlurAPI(
-    '0.0.0',
+const API = new PlurAPI(
+    '0.0.1',
     'git://github.com/asimovian/plur.git',
-    'master'
+    'roylaurie/unstable'
 );
 
-return PlurAPI.singleton;
-});
+
+export default API;
