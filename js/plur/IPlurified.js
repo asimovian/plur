87984f47586a9f931b57c46ed5f680ff43bf9816
roylaurie <roy.laurie@gmail.com>
2019-09-21 15:37:33 -0700

    iplurified and import/export fixes

--- js/plur/IPlurified.js
+++ js/plur/IPlurified.js
@@ -10,14 +10,14 @@
  *                 interface class with a value of PlurObject.abstractMethod is considered contract and inherited if not
  *                 already overridden. Static Interfaces are allowed and enforced by simply creating a static property
  *                 in the interface class.
- *   - Reflection: Albeit rudimentary, reflection is possible through universal class identification. Each class
- *                 provides a static and instance copy of its module path, known as a "namepath".
+ *
+ *   - Class Identity: Each class provides a static and instance copy of its module path, known as a "namepath".
  *
  * The PlurObject utility class provides all of the methods required to use plur psuedo language features.
  *
  * Implementing classes must:
- *   - Provide the module path as an immutable static property and instance property copy: "namepath".
- *   - Provide a array property named "implemented".
+ *   - Provide the module path as an immutable static property and prototype property copy: "namepath".
+ *   - Provide a static array property named "implemented".
  *   - All of this should be done by using PlurObject.plurify() after class declaration.
  *   - Export the class as the default export.
  *
