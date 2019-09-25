ddbb9e267b270f63f8b96078ad650c2109517cd6
roylaurie <roy.laurie@gmail.com>
2019-09-25 10:40:59 -0700

    nginx package on setup

--- bin/setup/setup-sdk-system.bash
+++ bin/setup/setup-sdk-system.bash
@@ -9,8 +9,8 @@ fi
 ## Removes the default nodejs package, if installed.
 apt-get remove nodejs
 
-# Installs build tools required for later steps. SnapD package manager.
-apt-get install build-essential git snapd
+# Installs build tools required for later steps. SnapD package manager. nginx for testing.
+apt-get install build-essential git snapd nginx
 
 # Installs snap package for Node.js v12
 snap install node --classic --channel=12
@@ -21,6 +21,7 @@ snap install webstorm --classic
 
 # Install the Google Closure Compiler as a system-wide tool
 npm install --global google-closure-compiler
+
 
 # there is no longer a nodejs executable, just node. the installed script simply redirects to the snap executable.
 # the silver lining is that, on this system, there's now a clear distinction between legacy (node) and es6m (nodejs).
