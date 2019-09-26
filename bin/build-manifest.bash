410b161dfd3e48f1e7f73351db6c1f16169885fc
roylaurie <roy.laurie@gmail.com>
2019-09-26 11:13:48 -0700

    removing /build

--- bin/build-manifest.bash
+++ bin/build-manifest.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-manifest='./js/plur/manifest.mjs'
+manifest='./build/plur-manifest.txt'
+jsManifest='./js/plur/manifest.mjs'
 echo 'export default [' > $manifest
 
 find ./js -name '*.mjs' | sed -E 's/^.\/js\///g' | sed -E 's/^/    "/g' | sed -E 's/$/",/g' >> $manifest
