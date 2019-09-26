eecef97f485bd4768f5a233d5501f8c025aeb0b3
roylaurie <roy.laurie@gmail.com>
2019-09-26 12:51:20 -0700

    plur bash lib fix

--- bin/plur.lib.bash
+++ bin/plur.lib.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 # @copyright 2019 Asimovian LLC
-# @
+# @license MIT https://github.com/asimovian/plur/blob/master/LICENSE.txt
+#
 # Generic plur bash script library
 
 # @param string dir $1
 function is_plur_homedir {
-	[ $(basename $(realpath $1)) == 'plur' ] # && cat package.json | grep -E '^\s+"name":\s+"\@plur\/plur",$'
+	[ $(basename $(realpath $1)) == 'plur' ] && cat package.json | grep -E '^\s+"name":\s+"\@plur\/plur",$' 1>/dev/null
 	return $?
 }
 
