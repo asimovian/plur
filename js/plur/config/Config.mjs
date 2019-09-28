9d3eb2a9601d6dd31ece828054bc16690678dd38
roylaurie <roy.laurie@gmail.com>
2019-09-27 22:29:09 -0700

    config schema drafting. needs a new class. deleted the map stuff.

--- js/plur/config/Config.mjs
+++ js/plur/config/Config.mjs
@@ -8,26 +8,44 @@
 import PlurObject from '../../plur/PlurObject.mjs';
 
 /**
- * Maintains key/value configuration for a subject object, typically for a prototype.
+ * Maintains nested key/value configuration for a subject class.
  *
  * Schema options:
- *   __ Configures a {} record schema. Value is the schema record.
+ *   _s Configures the entire schema.
+ *   Schema keys:
+ *   c {Array<string>} class The [alias, namepath] of a custom schema parser to load.
+ *                           Must implement IConfigParser.parseSchema().
+ *
+ *   _d Configures a {} data record schema. Value is the schema record.
+ *   Record schema keys: none
+ *   g See property schema.
+ *
+ *   _o Configures the encapsulating object or array.
+ *   Scope schema keys:
+ *   g See property schema.
+ *
  *   _ Configures a property schema when used as a record. Value is default value (v) or else an expected class/type (t).
- *   t {Class} type Field is instanceof type.
+ *   Property schema keys:
+ *   t {type} type The primitive type to expect (string, number, boolean, null, float}
  *   v {*} value Default value.
  *   r {boolean} required Field is required (true) or optional (false). The default is optional.
  *   p {RegExp} pattern Pattern to check strings against.
- *   g {Array<number>} range Valid numerical range where g[0] <= v <= g[1]
+ *   g {Array<number>} range Valid numerical range where g[0] <= v <= g[1]. Length when applied to strings and arrays.
+ *   c {string} Reference a previously declared schema parser by its alias.
  *
  ** Example:
  * new Config(MyClass, {
+ *     {_s:{ c:['password','plur/config/parser/Password']}} // loads a schema parser and gives it the alias 'password'
  *     apps: [
+ *         // the scope schema below makes 'apps' a required field and it must have at least 1 entry
+ *         {_o g:[1,],r:true},
  *         // schema below expects each record to have a required string namepath property and an optional
  *         // cmdlineOptions string
- *         {__:{ namepath: {_:String,r:true}, cmdlineOptions: String }} j
+ *         {_d:{ namepath: {_:string,r:true}, cmdlineOptions: String }} j
  *     ],
  *     bestNumber: 6,  // schema expects numbers, required, default value of 6
- *     secondNumber: {_:float,v:5},  // schema expects a float, default value of 5(.0)
+ *     secondNumber: {_:float,v:5},  // schema expects an optional float, default value of 5(.0)
+ *     password: {_:string,c:password,g:[4,12],r:true}  // required 4-12 char string passed to custom 'password' parser
  * };
  *
  * @final
@@ -73,15 +91,18 @@ class Config {
      * @param {!Object|!Config} config
      */
     constructor(configurable, config) {
+        /** @type {string} **/
         this._configurableNamepath = configurable.namepath;
+        /** @type {Object<string,(string|number|boolean|Object|Array|null)>} **/
         this._schema = null;  // classes only
+        /** @type {Object<string,(string|number|boolean|Object|Array|null)>} **/
         this._config = null;
 
         if (!PlurObject.implementing(configurable, IConfigurable)) {
             throw new Error('Cannot configure for a non-configurable class. Implement IConfigurable.');
         }
 
-        if (typeof configurable === 'Function') {  // it's a class
+        if (typeof configurable === 'function') {  // it's a class
             const parentClass = Object.getPrototypeOf(configurable);
 
             if (PlurObject.implementing(parentClass, IConfigurable)) {  // it has a parent config. make a merge copy.
