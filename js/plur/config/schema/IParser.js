c49cc9f82abe23b1d7e0c2bd13af8625fd28e638
roylaurie <roy.laurie@gmail.com>
2019-10-18 08:01:11 -0700

    3: rename plurobject.mjs

--- js/plur/config/schema/IParser.js
+++ js/plur/config/schema/IParser.js
@@ -6,7 +6,7 @@
  */
 'use strict';
 
-import PlurObject from '../../../plur/PlurObject.mjs';
+import PlurObject from '../../../plur/Class.mjs';
 import InterfaceError from '../../../plur/error/Interface';
 
 /**
--- js/plur/model/ITransformer.js
+++ js/plur/model/ITransformer.js
@@ -5,7 +5,7 @@
  */
 'use strict';
 
-import PlurObject from 'plur/PlurObject.mjs';
+import PlurObject from 'plur/Class.mjs';
 import InterfaceError from 'plur/error/Interface';
 
 /**
